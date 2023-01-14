e401cdac7a19c5c2b1f623b4784a405e85f6031e
Milan <milanstojanovski1@gmail.com>
2023-01-14 13:18:05 +0100

    swagger added

--- NBP_Project_2023/Server/Controllers/Neo4jController.cs
+++ NBP_Project_2023/Server/Controllers/Neo4jController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Neo4j.Driver;
+using System.Text;
 
 namespace NBP_Project_2023.Server.Controllers
 {
@@ -7,5 +9,24 @@ namespace NBP_Project_2023.Server.Controllers
     [ApiController]
     public class Neo4jController : ControllerBase
     {
+        private readonly IDriver _driver;
+
+        public Neo4jController()
+        {
+            _driver = GraphDatabase.Driver("bolt://localhost:7687", AuthTokens.Basic("neo4j", "password"));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateNode(string name)
+        {
+            var statementText = new StringBuilder();
+            statementText.Append("CREATE (person:Person {name: $name})");
+            var statementParameters = new Dictionary<string, object>
+            { { "name", name } };
+
+            var session = this._driver.AsyncSession();
+            var result = await session.WriteTransactionAsync(tx => tx.RunAsync(statementText.ToString(), statementParameters));
+            return StatusCode(201, "Node has been created in the database.");
+        }
     }
 }
